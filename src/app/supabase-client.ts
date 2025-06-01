a8561a7bbc4dc95d4aa57cb28a2ce7a8cc12ab23
USER <mireya.garcia@epn.edu.ec>
2025-06-01 18:17:19 -0500

    Se utiliza las variables de entorno

--- src/app/supabase-client.ts
+++ src/app/supabase-client.ts
@@ -1,6 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
+import { environment } from '../environments/environment';
 
-const supabaseUrl = 'https://sodlregonixbebwnvdxf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvZGxyZWdvbml4YmVid252ZHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyOTcyNTgsImV4cCI6MjA2Mzg3MzI1OH0.eyan4TXu8A1vo5YkedqofqvgC_NvmEkkgbBIXHGndak';
+const supabaseUrl = environment.supabaseUrl;
+const supabaseKey = environment.supabaseKey;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://sodlregonixbebwnvdxf.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvZGxyZWdvbml4YmVid252ZHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyOTcyNTgsImV4cCI6MjA2Mzg3MzI1OH0.eyan4TXu8A1vo5YkedqofqvgC_NvmEkkgbBIXHGndak'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
